751c250e65eaabcc0cea99ef989e68b2fafec09e
dspcrypto <dspcrypto@gmail.com>
2019-02-10 01:37:39 +0800

    added hover color for sending unit dropdown

--- src/qt/bitcoinamountfield.cpp
+++ src/qt/bitcoinamountfield.cpp
@@ -32,7 +32,7 @@ BitcoinAmountField::BitcoinAmountField(QWidget *parent):
     int width = unit->minimumSizeHint().width();
     unit->setMinimumWidth(width);
 
-    unit->setStyleSheet("QComboBox { background-color: #d2d2d2; }");
+    unit->setStyleSheet("QComboBox:selected { background-color: #78a539; }");
 
 
 
